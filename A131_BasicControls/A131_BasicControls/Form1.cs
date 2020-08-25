a87cc8f1bf6a8ffcbd6ebb8a363714646d59e41a
Madang Garden <chooi9522@gmail.com>
2020-08-25 15:28:21 +0900

    20.08.25

---
+++ A131_BasicControls/A131_BasicControls/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A131_BasicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                MessageBox.Show("Enter your name", "Warning");
+            else
+                label2.Text = textBox1.Text + ", Hi!";
+        }
+    }
+}
